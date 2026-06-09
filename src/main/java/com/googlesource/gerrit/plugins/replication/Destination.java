7c9a80f4b0b1f64439a0a658d3d62b3c1afe7417
Adithya Chakilam <achakilam@nvidia.com>
2026-06-09 15:45:18 -0500

    Destination: remove unused method
    Change-Id: I97fb13b03694572c1925f65abdc941cffd9fbe8b

--- src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
@@ -435,10 +435,6 @@ public class Destination {
     return false;
   }
 
-  void schedule(Project.NameKey project, Set<String> refs, URIish uri, ReplicationState state) {
-    schedule(project, refs, uri, state, false);
-  }
-
   void scheduleFromStorage(
       Project.NameKey project, Set<String> refs, URIish uri, ReplicationState state) {
     schedule(project, refs, uri, state, false, true);
